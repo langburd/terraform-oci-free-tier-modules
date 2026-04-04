89beb6851ff840d272730a607e8ca057090b04dd
Avi Langburd <avi@langburd.com>
2026-04-04 13:12:19 +0300

    chore(examples): remove two-profiles and two-tenancies (replaced by dual-tenancy)

--- examples/two-profiles/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "compartment1_id" {
-  description = "The OCID of the compartment for profile 1"
-  value       = module.compartment1.compartment_id
-}
-
-output "compartment2_id" {
-  description = "The OCID of the compartment for profile 2"
-  value       = module.compartment2.compartment_id
-}
